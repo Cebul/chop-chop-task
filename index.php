6c266ff5f9a65bc0f1eed77e608cbb8259ae5ae6
kamil <kamilcebula1@gmail.com>
2017-08-01 16:00:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chop-chop Task</title>
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="shortcut icon" type="image/png" href="styles/img/sample-pack.png">
+    <link rel="stylesheet" href="./styles/font-awsome/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li class="home-nav"><a href="#">Home</a></li>
+          <li><a href="#">Products</a></li>
+          <li id="logo"><img src="./styles/img/sample-pack.png" alt="Sample&Pack"></li>
+          <li><a href="#">About Us</a></li>
+          <li class="contact-nav"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section id="main-title">
+      <h1>This is just a sample page</h1>
+      <button type="button"  id="main-button" name="push-the-button">push the button</button>
+    </section>
+    <main>
+      <article class="text-block">
+        <h1>A heading title</h1>
+        <img src="./styles/img/text-column-img.png" alt="Code Picture">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        At elementum eu facilisis sed odio. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Neque aliquam vestibulum morbi blandit cursus risus. Sed odio morbi quis commodo odio aenean. In eu mi bibendum neque egestas. Posuere ac ut consequat semper viverra.
+      </article>
+      <article class="text-block">
+        <h1>A much longer heading title</h1>
+        <img src="./styles/img/text-column-img.png" alt="Code Picture">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        At elementum eu facilisis sed odio. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Neque aliquam vestibulum morbi blandit cursus risus. Sed odio morbi quis commodo odio aenean. In eu mi bibendum neque egestas. Posuere ac ut consequat semper viverra.
+      </article>
+      <article class="text-block">
+        <h1>A heading title</h1>
+        <img src="./styles/img/text-column-img.png" alt="Code Picture">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        At elementum eu facilisis sed odio. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Neque aliquam vestibulum morbi blandit cursus risus. Sed odio morbi quis commodo odio aenean. In eu mi bibendum neque egestas. Posuere ac ut consequat semper viverra.
+      </article>
+    </main>
+    <section id="contact-us">
+
+    </section>
+    <footer>
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+    </footer>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="./js/index.js"></script>
+  </body>
+</html>
